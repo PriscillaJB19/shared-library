86e9e560eac0f832b6f86a3596c6623defba19dd
PriscillaJB19 <110067340+PriscillaJB19@users.noreply.github.com>
2022-09-25 19:48:01 -0600

    Update Build.groovy

--- vars/Build.groovy
+++ vars/Build.groovy
@@ -1,4 +1,4 @@
 def call (Map config = [:]){
-sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
+sh "echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin"
 sh "docker build -t ${config.registry}:${config.buildNumber} . "
 }
