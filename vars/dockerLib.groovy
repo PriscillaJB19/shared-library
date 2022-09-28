7d4f8dd8393d815f3e69e71f0bf09bcd48bc9a2f
PriscillaJB19 <110067340+PriscillaJB19@users.noreply.github.com>
2022-09-28 08:02:11 -0600

    Create dockerLib.groovy

---
+++ vars/dockerLib.groovy
@@ -0,0 +1,14 @@
+def build(Map params){
+    sh "docker build -f ${params.DockerfilePath} -t ${params.DockerImage} ${params.DockerContext}"
+}
+
+
+def push(Map params){
+    sh "docker push ${params.DockerImage}"
+}
+
+def promoter(Map params){
+    sh "docker pull ${params.DockerImage}"
+    sh "docker tag ${params.DockerImage} ${params.DockerNewImage}"
+    sh "docker push ${params.DockerNewImage}"
+}
