9b7ebc8cc64d82be58eda032dc2c78c6794fd0a9
PriscillaJB19 <110067340+PriscillaJB19@users.noreply.github.com>
2022-09-28 16:29:29 -0600

    Update Build.groovy

--- vars/Build.groovy
+++ vars/Build.groovy
@@ -1,3 +1,3 @@
 def call (Map config = [:]){
-  sh "cd ./${config.proyect_root};docker build -t ${config.registry}:${config.buildNumber} ."
+  sh "cd ${config.proyect_root};docker build -t ${config.registry}:${config.buildNumber} ."
 }
