cf9028effa83556e793e9a03ca9bcc6f5dc7f1d8
PriscillaJB19 <110067340+PriscillaJB19@users.noreply.github.com>
2022-10-04 16:02:59 -0600

    Update Run.groovy

--- vars/Run.groovy
+++ vars/Run.groovy
@@ -1,5 +1,4 @@
 def call (Map config = [:]){
    sh "docker login -u priscillajb -p dckr_pat_FT0zDVqjuc0LvxCdHfNHkXg9Du4 docker.io"
-  // docker run -d --name Consola  -p 3000:3000 netconsola
    sh "docker run -d --name Node -p 3000:3000 ${config.registry}:${config.buildNumber}"   
 }
