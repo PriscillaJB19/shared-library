d4d04cc8539f7babbfb86e7e97e309d3d691b237
PriscillaJB19 <110067340+PriscillaJB19@users.noreply.github.com>
2022-10-04 09:25:22 -0600

    Update Push.groovy

--- vars/Push.groovy
+++ vars/Push.groovy
@@ -1,11 +1,5 @@
 def call (Map config = [:]){
    //sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR password stdin'
    //sh "docker login registry-1.docker.io"
-   //sh "docker push ${config.registry}:${config.buildNumber}"
-   
-   //sh "
-      docker.withRegistry('<priscillajb>', 'priscillajb-dockerhub') {
-            docker push ${config.registry}
-      }
-   
+   sh "docker push ${config.registry}:${config.buildNumber}"   
 }
